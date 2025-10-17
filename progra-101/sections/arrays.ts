b7f8b93f0e36614698f560d5da96bbd37efa6c3b
Wilson Guayanay <wilson.alex@outlook.es>
2025-10-17 16:54:42 -0500

    Arrays

---
+++ progra-101/sections/arrays.ts
@@ -0,0 +1,20 @@
+// Arreglos
+
+export let employees: string[] = ['Wilson', 'Alexander', ' Guayanay'];
+export let salaries: number[] = [1500, 2400, 3200];
+
+// let flowers: string[] = ['Rosa', 'Girasol', 'Lirio', 'Margarita'];
+
+// if (people[2]) {
+//     console.log(people[2]);
+// }
+
+
+// for(let i = 0; i < flowers.length; i++){
+//     console.log(flowers[i]);
+// }
+
+
+for (let i = 0; i < employees.length; i++) {
+    console.log(employees[i], ' tiene un salario de: ', salaries[i]);
+}
